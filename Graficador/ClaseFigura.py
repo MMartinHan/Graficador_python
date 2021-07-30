0eacdaf0da18b4a6684fd53aca5b3b10a2979b4f
MMartinHan <mmahan@espe.edu.ec>
2021-07-30 14:40:22 -0500

    primera subida

---
+++ Graficador/ClaseFigura.py
@@ -0,0 +1,70 @@
+#from tkinter import *
+
+
+#Crear ventana
+#ventana = Tk()
+#nombre de la ventana
+#ventana.title("Calculo Vectorial")
+#el ancho y el alto con cero se puede cambiar con 1 no se puede cambiar booleanos
+#ventana.resizable(0,0)
+# ventana.iconbitmap("ruta de la imagen ico")
+#ventana.geometry("600x400") #Poner las dimensiones de la ventana
+#ventana.config(bg="green") #Cambiar el fondo de la ventana
+
+
+#Crear papel
+#Papel encima de ventana
+#papel = Frame(ventana, width = 800, height = 600 )
+#papel.pack() #agregamos a la ventana
+#Dimensiones al papel
+#papel.config(width = "800", height = "600") #Dimensiones del papel
+#papel.config(bg = "white") #Background del papel
+
+
+#Creacion de Label
+#fg para el color de la letra y font para el tamaño de la letra Video numero 3
+#miLabel = Label(papel, text = "Universidad de las Fuerzas Armadas ESPE", fg = "black", font = 15)
+#miLabel.place(x = 260, y = 20) #Empaquetarlo
+#miLabel.config(bg = "white")
+
+#ecuacion = Label(papel, text = "Ingrese su Ecuacion", fg = "black", font = 10)
+#ecuacion.place(x= 30, y = 100)
+#ecuacion.config(bg = "white")
+
+#dominio = Label(papel, text = "Ingrese el dominio",fg = "black", font = 10)
+#dominio.place(x = 335, y = 100)
+#dominio.config(bg = "white")
+
+#rango = Label(papel, text = "Ingrese el rango", fg = "black", font = 10)
+#rango.place(x = 650, y = 100)
+#rango.config(bg = "white")
+
+#IMAGEN
+#miImagen = PhotoImage(file = "imagenparapython.png")
+#imagenL = Label(papel, image = miImagen).place(x= 110, y = 200 )
+
+
+#Cuadros de Texto
+#cuadroTexto = Entry(papel)
+#cuadroTexto.place(x = 650, y = 130)
+#cuadroTexto.config(bg = "white")
+
+#cuadroTexto2 = Entry(papel)
+#cuadroTexto2.place(x = 335, y= 130)
+#cuadroTexto2.config(bg = "white")
+
+#cuadroTexto3 = Entry(papel)
+#cuadroTexto3.place(x = 30, y = 130)
+#cuadroTexto3.config(bg = "white")
+
+
+
+
+
+#Texto
+
+
+
+#Botones
+
+#ventana.mainloop()
